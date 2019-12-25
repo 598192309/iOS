32c0eb20679c3e25204fc45a77026aab8c24f057
lqq <15622709338@163.com>
2019-12-26 00:17:30 +0800

    a

---
+++ Bigjpg/Bigjpg/Managers/ConfManager.m
@@ -0,0 +1,13 @@
+//
+//  ConfManager.m
+//  Bigjpg
+//
+//  Created by 黎芹 on 2019/12/25.
+//  Copyright © 2019 lqq. All rights reserved.
+//
+
+#import "ConfManager.h"
+
+@implementation ConfManager
+
+@end
